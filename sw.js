17d6fadee5a9820e3f4ca8dce9f254dfe8d17e13
spardexlab-maker <spardexlab@gmail.com>
2026-08-29 13:22:27 +0300

    Initial commit: Household Chaos Court Web App for Ashraf and Sarah

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'chaos-court-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
